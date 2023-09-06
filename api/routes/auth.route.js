5124591025aca050cf08c50215b685cf27db3987
ashirbay-darkhan <darkhan.ashirbay01@gmail.com>
2023-09-06 17:57:56 +0600

    created sign in api routes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {sighup} from "../controllers/auth.controller.js";
+import {sighup, signin} from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
 router.post("/signup", sighup)
+router.post("/signin", signin)
 
 export default router
