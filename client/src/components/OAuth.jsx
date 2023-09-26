12b7b6df97113cb9e67050b6855c06700b7d0fcd
ashirbay-darkhan <darkhan.ashirbay01@gmail.com>
2023-09-27 03:23:05 +0600

    completed user update functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,8 +2,11 @@ import {GoogleAuthProvider, signInWithPopup, getAuth} from 'firebase/auth'
 import {app} from "../firebase.js";
 import {useDispatch} from "react-redux";
 import {signInSuccess} from "../redux/user/userSlice.js";
+import {useNavigate} from "react-router-dom";
 const OAuth = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider
@@ -24,6 +27,7 @@ const OAuth = () => {
       const data = await res.json()
       console.log(data)
       dispatch(signInSuccess(data))
+      navigate('/')
     } catch (error) {
       console.log(error)
     }
